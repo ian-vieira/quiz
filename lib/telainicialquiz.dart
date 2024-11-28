1a34f62c996bd8789a1e276f703d1956b8a8ecdb
ian-vieira <ian.vieira@ba.estudante.senai.br>
2024-11-28 18:30:19 -0300

    Update telainicialquiz.dart

--- lib/telainicialquiz.dart
+++ lib/telainicialquiz.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'telaquiz.dart'; // Importa a página do quiz
-import 'telainfo.dart'; // Importa a página de informações
+import 'telaquiz.dart';
+import 'telainfo.dart'; 
 
 class TelaInicial extends StatelessWidget {
   const TelaInicial({super.key});
@@ -10,19 +10,19 @@ class TelaInicial extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Bem-vindo ao Quiz App'),
-        backgroundColor: Colors.blue.shade100, // Cor do AppBar com azul bebê
+        backgroundColor: Colors.blue.shade100, 
       ),
       body: Container(
-        color: Colors.blue.shade100, // Cor de fundo da tela para azul bebê
+        color: Colors.blue.shade100, 
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Image.network(
                 'https://png.pngtree.com/png-clipart/20230811/original/pngtree-comic-speech-bubbles-with-text-quiz-picture-image_7868432.png',
-                width: 300, // Largura desejada
-                height: 300, // Altura desejada
-                fit: BoxFit.cover, // Ajusta a imagem para cobrir a área
+                width: 300, 
+                height: 300, 
+                fit: BoxFit.cover, 
               ),
               const Text(
                 'Bem-vindo ao Quiz!',
@@ -31,30 +31,30 @@ class TelaInicial extends StatelessWidget {
               const SizedBox(height: 20),
               ElevatedButton(
                 onPressed: () {
-                  // Navega para a página do quiz
+                 
                   Navigator.push(
                     context,
                     MaterialPageRoute(builder: (context) => QuizPage()),
                   );
                 },
                 style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.yellow, // Cor de fundo do botão (amarelo)
-                  foregroundColor: Colors.black, // Cor do texto (preto)
+                  backgroundColor: Colors.yellow, 
+                  foregroundColor: Colors.black, 
                 ),
                 child: const Text('Iniciar Quiz'),
               ),
               const SizedBox(height: 20),
               ElevatedButton(
                 onPressed: () {
-                  // Navega para a página de informações
+                  
                   Navigator.push(
                     context,
                     MaterialPageRoute(builder: (context) => InformacoesPage()),
                   );
                 },
                 style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.yellow, // Cor de fundo do botão (amarelo)
-                  foregroundColor: Colors.black, // Cor do texto (preto)
+                  backgroundColor: Colors.yellow, 
+                  foregroundColor: Colors.black,
                 ),
                 child: const Text('Informações'),
               ),
